6cb9ca74dd06ff8114a989b170b06f9e07492516
swksoft <esparzapatricio95@gmail.com>
2024-05-28 16:30:47 -0300

    ui update (now with more separated scenes)

--- ui/ui.gd
+++ ui/ui.gd
@@ -3,26 +3,19 @@ extends Control
 @onready var stats_panel = $CanvasLayer/StatsPanel
 @onready var shop = $CanvasLayer/Shop
 @onready var tutorial_screen = $CanvasLayer/TutorialScreen
-@onready var turn_label = $CanvasLayer/HBoxContainer2/TurnLabel
-@onready var money_label = $CanvasLayer/HBoxContainer2/MoneyLabel
 
 func _ready():
-	pass #
+	GameEvents.emit_update_general_data()
 
 func _on_button_mouse_entered():
-	# TODO: SHOW DATA
+	GameEvents.emit_data_troup()
 	stats_panel.visible = true
-	print("Info opened")
 
 func _on_button_mouse_exited():
-	# TODO: HIDE DATA
 	stats_panel.visible = false
-	print("Info closed")
-
 
 func _on_add_button_pressed():
 	shop.visible = true
-
 
 func _on_button_pressed():
 	tutorial_screen.visible = true
