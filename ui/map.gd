b9747017f9ff1982ae3d9bc62b71cdca1e241152
swksoft <esparzapatricio95@gmail.com>
2024-05-28 16:31:51 -0300

    map changes (for better)

--- ui/map.gd
+++ ui/map.gd
@@ -1,16 +1,1 @@
 extends Control
-
-func _on_take_action_0_pressed():
-	print("Go to Area 0")
-
-func _on_take_action_1_pressed():
-	print("Go to Area 1")
-
-func _on_take_action_2_pressed():
-	print("Go to Area 2")
-
-func _on_take_action_3_pressed():
-	print("Go to Area 3")
-
-func _on_take_action_4_pressed():
-	print("Go to Area 4")
