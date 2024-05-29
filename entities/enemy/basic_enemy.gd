78b14aeaa4b1c3edfc9edecf7060f14741916ccf
swksoft <esparzapatricio95@gmail.com>
2024-05-29 16:38:16 -0300

    basic enemy changes

--- entities/enemy/basic_enemy.gd
+++ entities/enemy/basic_enemy.gd
@@ -1,19 +1,46 @@
 extends CharacterBody2D
 class_name Enemy
 
-@export var speed = 160
+@export var speed : int = 160
 @export var direction = Vector2.LEFT
-@export var money_get = 15
+@export var money_get = 5
+@export var randomized : bool = true
+
+@export var bullet_scene : PackedScene
+
+func _ready():
+	randomize()
+	
+	if randomized == true:
+		speed = randi_range(50, 250)
 
 func _physics_process(delta):
 	position += direction * speed * delta
 
 func get_damage():
-	GameEvents.current_money
+	GameEvents.current_money += money_get
 	queue_free()
 
 func _on_hurtbox_area_entered(area):
 	if area.is_in_group("PlayerBullet"):
+		area.pierce += 1
+		if area.pierce >= area.max_pierce: area.queue_free()
 		get_damage()
-	area.queue_free()
+		
+	elif area.is_in_group("Troop"):
+		get_damage()
+		
+	elif area.is_in_group("Explosion"):
+		get_damage()
 
+func shoot():
+	var bullet = bullet_scene.instantiate()
+	
+	bullet.position = global_position
+	bullet.set_collision_layer_value(2, true)
+	bullet.direction = Vector2.LEFT
+	
+	get_parent().add_child(bullet)
+
+func _on_timer_timeout():
+	shoot()
