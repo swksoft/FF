0d195e046e4046332693dab468678802f3b3af90
swksoft <esparzapatricio95@gmail.com>
2024-05-29 16:36:58 -0300

    New projectiles + Tweaks

--- entities/bullet/bullet.gd
+++ entities/bullet/bullet.gd
@@ -4,16 +4,20 @@ class_name Bullet
 @export var time_despawn : float = 2.0
 @export var speed : int = 500
 @export var trail_lenght = 5
+@export var damage = 1
+@export var max_pierce = 3
+
+var pierce : int = 0
+var direction : Vector2 = Vector2.RIGHT
 
 @onready var despawn = $Despawn
 @onready var trails = $Trails
 
 func _ready():
-	trails.MAX_LENGHT = trail_lenght
 	despawn.wait_time = time_despawn
 
 func _process(delta):
-	position += Vector2.RIGHT * speed * delta
+	position += direction * speed * delta
 
 func _on_despawn_timeout():
 	queue_free()
