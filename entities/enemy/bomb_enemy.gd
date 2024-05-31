2d3a1febfe34362e050da24ead7df52f7b171b52
Widenyo <widenyo@national.shitposting.agency>
2024-05-30 22:18:28 -0300

    bomb explodes on contact

--- entities/enemy/bomb_enemy.gd
+++ entities/enemy/bomb_enemy.gd
@@ -6,6 +6,10 @@ func get_damage():
 	var explosion = explosion_scene.instantiate()
 	explosion.position = global_position
 	get_parent().call_deferred("add_child", explosion)
+	death()
 
 func _on_explosion_time_timeout():
+	get_damage()
+
+func _on_hurtbox_area_entered(_area):
 	get_damage()
